3d47c5b5249896c41116c22777597cd6c1301233
criegt <criegt@outlook.com>
2020-11-13 20:44:44 -0500

    Agrega primer test de la Kata.

---
+++ src/BowlingGame/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace BowlingGame
+{
+    public class Game
+    {
+        public int Score => 0;
+
+        public void Roll(int pins) { }
+    }
+}
---
+++ test/BowlingGame.Tests/BowlingGameTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace BowlingGame.Tests
+{
+    public class BowlingGameTest
+    {
+        [Fact]
+        public void TestGutterGame()
+        {
+            var game = new Game();
+
+            for (var rollCount = 0; rollCount < 20; rollCount++)
+            {
+                game.Roll(0);
+            }
+
+            Assert.Equal(0, game.Score);
+        }
+    }
+}
