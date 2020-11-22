d50c5e502101a765be1cd74fa13cfe9681265a35
criegt <criegt@outlook.com>
2020-11-22 17:39:26 -0500

    Agrega cuarto test de kata.

--- src/BowlingGame/Game.cs
+++ src/BowlingGame/Game.cs
@@ -2,7 +2,7 @@
 {
     public class Game
     {
-        private const int Spare = 10;
+        private const int Strike = 10;
         private readonly int[] _rolls = new int[21];
         private int _currentRoll;
 
@@ -13,27 +13,52 @@
                 var score = 0;
                 var frameIndex = 0;
 
-                for (var frame = 0; frame < Spare; frame++)
+                for (var frame = 0; frame < Strike; frame++)
                 {
-                    if (IsSpare(frameIndex))
+                    if (IsStrike(frameIndex))
                     {
-                        score += Spare + _rolls[frameIndex + 2];
+                        score += Strike + StikeBonus(frameIndex);
+                        frameIndex++;
+                    }
+                    else if (IsSpare(frameIndex))
+                    {
+                        score += SpareBonus(frameIndex);
+                        frameIndex += 2;
                     }
                     else
                     {
-                        score += _rolls[frameIndex] + _rolls[frameIndex + 1];
+                        score += PinsInFrame(frameIndex);
+                        frameIndex += 2;
                     }
-
-                    frameIndex += 2;
                 }
 
                 return score;
             }
         }
 
+        private bool IsStrike(int frameIndex)
+        {
+            return _rolls[frameIndex] == 10;
+        }
+
+        private int PinsInFrame(int frameIndex)
+        {
+            return _rolls[frameIndex] + _rolls[frameIndex + 1];
+        }
+
+        private int SpareBonus(int frameIndex)
+        {
+            return Strike + _rolls[frameIndex + 2];
+        }
+
+        private int StikeBonus(int frameIndex)
+        {
+            return _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+        }
+
         public void Roll(int pins) => _rolls[_currentRoll++] = pins;
 
         private bool IsSpare(int frameIndex)
-            => _rolls[frameIndex] + _rolls[frameIndex + 1] == Spare;
+            => _rolls[frameIndex] + _rolls[frameIndex + 1] == Strike;
     }
 }
--- test/BowlingGame.Tests/BowlingGameTest.cs
+++ test/BowlingGame.Tests/BowlingGameTest.cs
@@ -25,14 +25,6 @@ namespace BowlingGame.Tests
             Assert.Equal(20, _game.Score);
         }
 
-        private void RollManyTimes(int timesToRoll, int pins)
-        {
-            for (var i = 0; i < timesToRoll; i++)
-            {
-                _game.Roll(pins);
-            }
-        }
-
         [Fact]
         public void TestOneSpare()
         {
@@ -40,6 +32,36 @@ namespace BowlingGame.Tests
             _game.Roll(3);
             RollManyTimes(17, 0);
             Assert.Equal(16, _game.Score);
+        }
+
+        [Fact]
+        public void TestOneStrike()
+        {
+            RollStrike();
+            _game.Roll(3);
+            _game.Roll(4);
+            RollManyTimes(16, 0);
+            Assert.Equal(24, _game.Score);
+        }
+
+        [Fact]
+        public void TestPerfectGame()
+        {
+            RollManyTimes(12, 10);
+            Assert.Equal(300, _game.Score);
+        }
+
+        private void RollStrike()
+        {
+            _game.Roll(10);
+        }
+
+        private void RollManyTimes(int timesToRoll, int pins)
+        {
+            for (var i = 0; i < timesToRoll; i++)
+            {
+                _game.Roll(pins);
+            }
         }
 
         private void RollSpare()
